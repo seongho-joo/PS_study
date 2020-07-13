d43c409fa33ddc80459dcadce3957309cc5e1647
seongho-joo <hazzyseong@gmail.com>
2020-07-13 11:46:26 +0900

    BOJ5086 배수와 약수 : 구현, 수학

---
+++ BOJ/3054.cpp
@@ -0,0 +1,48 @@
+//
+//  3054.cpp
+//  test
+//
+//  Created by 주성호 on 2020/07/09.
+//  Copyright © 2020 주성호. All rights reserved.
+//
+//  https://www.acmicpc.net/problem/3054
+//  BOJ3054 피터팬 프레임 : 구현
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str, result[5];
+    cin >> str;
+    
+    result[0] = "..#..";
+    for(int i = 1; i < str.length(); i++) {
+        if(i % 3 == 2) result[0] += ".*..";
+        else result[0] += ".#..";
+    }
+    result[4] = result[0];
+    
+    result[1] = ".#.#.";
+    for(int i = 1; i < str.length(); i++) {
+        if(i % 3 == 2) result[1] += "*.*.";
+        else result[1] += "#.#.";
+    }
+    result[3] = result[1];
+    
+    result[2] = "#.";
+    result[2] += str[0];
+    result[2] += ".#";
+    for(int i = 1; i < str.length(); i++) {
+        result[2] += ".";
+        result[2] += str[i];
+        if(i % 3 == 2) result[2] += ".*";
+        else if (i % 3 == 1) {
+            if(i == str.length() - 1) result[2] += ".#";
+            else result[2] += ".*";
+        }
+        else result[2] += ".#";
+    }
+    for(int i = 0; i < 5; i++) cout << result[i] << "\n";
+    return 0;
+}
